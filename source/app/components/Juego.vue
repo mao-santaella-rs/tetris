9cbeb36cbd3139775022ccba695b58e310d36c6e
Mao <mao8a87@gmail.com>
2019-05-16 09:36:06 -0500

    inicio

--- source/app/components/Juego.vue
+++ source/app/components/Juego.vue
@@ -2,12 +2,38 @@
 
 #juego
 	h1 Hola Mao
+	Tetronomo
 
 </template>
 
 <script>
+import Tetronomo from "./Tetronomo"
 export default {
-	name: "Juego"
+	name: "Juego",
+	components:{
+		Tetronomo
+	},
+	data(){
+		return{
+			tablero:[]
+		}
+	},
+	mounted(){
+		this.armarTablero()
+	},
+	methods:{
+		armarTablero(){
+			let columnas = 10
+			let filas = 20
+			for(let fila = 1; fila <= filas; fila++){
+				let filaP = []
+				for(let col = 1; col <= columnas; col++){
+					filaP.push(0)
+				}
+				this.tablero.push(filaP)
+			}
+		}
+	}
 }
 </script>
 
---
+++ source/app/components/Tetronomo.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+.tetronomo
+	.tet-fila(
+		v-for="fila in j.forma"
+	)
+		.tet-block(
+			v-for="block in fila"
+			:class="{fill : block == 1}"
+		)
+
+</template>
+
+<script>
+export default {
+	name: "Tetronomo",
+	data(){
+		return{
+			j:{
+				forma: [
+					[0,0,1,0],
+					[0,0,1,0],
+					[0,1,1,0],
+					[0,0,0,0]
+				],
+				color: "rojo"
+			}
+		}
+	}
+}
+</script>
+
+<style lang="sass">
+.tet-fila
+	display: flex
+.tet-block
+	height: 20px
+	width: 20px
+
+.fill
+	background-color: red
+
+</style>
